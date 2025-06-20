c60f5ed687726821526f1cc6e9887b2bd6e75c01
Pedro Teixeira <pedro.teixeira@formacerta.com.br>
2025-06-20 12:35:07 -0300

    feature: adding repositories and UnitOfWork;

---
+++ Bookify.Domain/Users/IUserRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace Bookify.Domain.Users;
+
+public interface IUserRepository
+{
+    Task<User> GetById(Guid id, CancellationToken cancellationToken = default);
+    Task Add(string email);
+}
