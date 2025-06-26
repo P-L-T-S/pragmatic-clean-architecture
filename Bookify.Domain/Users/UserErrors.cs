e331e2500e583561502b5fa55eddc85d42694e34
Pedro Teixeira <pedro.teixeira@formacerta.com.br>
2025-06-26 08:46:24 -0300

    feature: adding ApartmentErrors.cs and UserErrors.cs;

---
+++ Bookify.Domain/Users/UserErrors.cs
@@ -0,0 +1,11 @@
+﻿using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.Users;
+
+public static class UserErrors
+{
+    public static Error NotFound = new(
+        "User.Found",
+        "The user with the specified ID was not found."
+    );
+}
