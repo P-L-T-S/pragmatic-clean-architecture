6a468ba97fff5bc4e3ebe2b48cbabe59786fe196
Pedro Teixeira <pedro.teixeira@formacerta.com.br>
2025-06-26 11:31:53 -0300

    feature: implements the get booking query and get booking query handler;

--- Bookify.Domain/Bookings/IBookingRepository.cs
+++ Bookify.Domain/Bookings/IBookingRepository.cs
@@ -4,7 +4,7 @@ namespace Bookify.Domain.Bookings;
 
 public interface IBookingRepository
 {
-    Task<Booking> GetById(Guid id, CancellationToken cancellationToken = default);
+    Task<Booking?> GetById(Guid id, CancellationToken cancellationToken = default);
     Task Add(Booking booking, CancellationToken cancellationToken = default);
     Task<bool> IsOverLapping(Apartment apartment, DateRange duration, CancellationToken cancellationToken = default);
 }
